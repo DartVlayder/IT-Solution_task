3bce9e6982d6afe278206d5a4e2b893c63a5ea40
DartVlayder <mr.6990@mail.ru>
2025-07-12 18:18:57 +0300

    Добавлена модель Quote. Выполнены миграции.

--- ITSolution_task/quotes/models.py
+++ ITSolution_task/quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    text = models.TextField(unique=True)
+    source = models.CharField()
+    weight = models.PositiveIntegerField(default=1)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.text} из {self.source}'
