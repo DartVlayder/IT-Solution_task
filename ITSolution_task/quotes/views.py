c32fbfe18e53dd3ea46925236f71b2a159851c17
DartVlayder <mr.6990@mail.ru>
2025-07-15 18:59:24 +0300

    добавлены лайки и дизлайки, стили

--- ITSolution_task/quotes/views.py
+++ ITSolution_task/quotes/views.py
@@ -7,18 +7,35 @@ def index(request):
     quotes = list(Quote.objects.all())
     quote = None
 
-    if quotes:
-        quote = random.choices(quotes, weights=[q.weight for q in quotes])[0]
-        quote.views += 1
-        quote.save()
-
     if request.method == 'POST':
+        if 'like_id' in request.POST:
+            quote_id = request.POST['like_id']
+            quote_to_like = Quote.objects.get(id=quote_id)
+            quote_to_like.likes += 1
+            quote_to_like.save()
+            return redirect('index')
+
+        elif 'dislike_id' in request.POST:
+            quote_id = request.POST['dislike_id']
+            quote_to_dislike = Quote.objects.get(id=quote_id)
+            quote_to_dislike.dislikes += 1
+            quote_to_dislike.save()
+            return redirect('index')
+
         form = QuoteForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('index')
+        else:
+            if quotes:
+                quote = random.choices(quotes, weights=[q.weight for q in quotes])[0]
+
     else:
         form = QuoteForm()
+        if quotes:
+            quote = random.choices(quotes, weights=[q.weight for q in quotes])[0]
+            quote.views += 1
+            quote.save()
 
     return render(request, 'quotes/index.html', {
         'quote': quote,
