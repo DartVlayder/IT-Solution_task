0596006ef66f70a90885dddabc4299c0b5f3655b
DartVlayder <mr.6990@mail.ru>
2025-07-15 13:19:04 +0300

    Реализовал вывод случайной цитаты и подсчёт просмотров

--- ITSolution_task/quotes/views.py
+++ ITSolution_task/quotes/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Quote
+import random
 
 def index(request):
-    return render(request, 'quotes/index.html')
+    quotes = list(Quote.objects.all())
+
+    if quotes:
+        quote = random.choices(quotes, weights=[q.weight for q in quotes])[0]
+        quote.views += 1
+        quote.save()
+    else:
+        quote = None
+
+    return render(request, 'quotes/index.html', {'quote': quote})
